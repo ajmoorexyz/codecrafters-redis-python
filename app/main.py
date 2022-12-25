06bf13fdb5a60e6004f12af71eadc5ff11d73429
AJ Moore <ajmoore.xyz@gmail.com>
2022-12-24 17:12:30 -0700

    pass 2nd stage

--- app/main.py
+++ app/main.py
@@ -3,7 +3,8 @@ import socket
 
 def main():
     server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
-    server_socket.accept()  # wait for client
+    client, addr = server_socket.accept()  # wait for client
+    client.send(b"+PONG\r\n")
 
 
 if __name__ == "__main__":
