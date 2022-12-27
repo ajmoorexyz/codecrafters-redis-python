c49a1d8371332ddcb145c53748a405f67c76a16c
AJ Moore <ajmoore.xyz@gmail.com>
2022-12-27 10:26:40 -0700

    pass 5th stage

--- app/main.py
+++ app/main.py
@@ -1,18 +1,22 @@
 import socket
+import threading
+
+
+def handle_connection(client_connection):
+    while True:
+        try:
+            client_connection.recv(1024)  # receive data from client
+            client_connection.send(b"+PONG\r\n")
+        except ConnectionError:
+            break
 
 
 def main():
     socket.socket()
     server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
-    server_socket.listen(3)  # listen for clients
-    client, _ = server_socket.accept()  # wait for client
     while True:
-        data = client.recv(1024).decode()  # receive data from client
-        if not data:
-            break
-        client.send(b"+PONG\r\n")
-
-    client.close()  # close the socket
+        client_connection, _ = server_socket.accept()  # wait for client
+        threading.Thread(target=handle_connection, args=(client_connection,)).start()
 
 
 if __name__ == "__main__":
