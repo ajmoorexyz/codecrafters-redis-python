c7a3b63be5957a497d17213eb7cd499145ed096e
AJ Moore <ajmoore.xyz@gmail.com>
2022-12-27 04:15:50 -0700

    pass 3rd stage

--- app/main.py
+++ app/main.py
@@ -2,9 +2,16 @@ import socket
 
 
 def main():
+    socket.socket()
     server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
-    client, addr = server_socket.accept()  # wait for client
-    client.send(b"+PONG\r\n")
+    client, _ = server_socket.accept()  # wait for client
+    while True:
+        data = client.recv(1024).decode()  # receive data from client
+        if not data:
+            break
+        client.send(b"+PONG\r\n")
+
+    client.close()  # close the socket
 
 
 if __name__ == "__main__":
