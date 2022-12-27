95520362b30e575d54b378473877139a8ebeedce
AJ Moore <ajmoore.xyz@gmail.com>
2022-12-27 11:19:59 -0700

    debug 5th step

--- app/main.py
+++ app/main.py
@@ -1,22 +1,61 @@
+import sys
 import socket
-import threading
+import selectors
+import types
+
+sel = selectors.DefaultSelector()
 
 
-def handle_connection(client_connection):
-    while True:
-        try:
-            client_connection.recv(1024)  # receive data from client
-            client_connection.send(b"+PONG\r\n")
-        except ConnectionError:
-            break
+def accept_wrapper(sock):
+    conn, addr = sock.accept()  # Should be ready to read
+    print(f"Accepted connection from {addr}")
+    conn.setblocking(False)
+    data = types.SimpleNamespace(addr=addr, inb=b"", outb=b"")
+    events = selectors.EVENT_READ | selectors.EVENT_WRITE
+    sel.register(conn, events, data=data)
+
+
+def service_connection(key, mask):
+    sock = key.fileobj
+    data = key.data
+    if mask & selectors.EVENT_READ:
+        recv_data = sock.recv(1024)  # Should be ready to read
+        if recv_data:
+            data.outb += recv_data
+        else:
+            print(f"Closing connection to {data.addr}")
+            sel.unregister(sock)
+            sock.close()
+    if mask & selectors.EVENT_WRITE:
+        if data.outb:
+            print(f"Echoing {data.outb!r} to {data.addr}")
+            # sent = sock.send(data.outb)  # Should be ready to write
+            sent = sock.send((b"+PONG\r\n"))  # Should be ready to write
+            data.outb = data.outb[sent:]
 
 
 def main():
-    socket.socket()
-    server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
-    while True:
-        client_connection, _ = server_socket.accept()  # wait for client
-        threading.Thread(target=handle_connection, args=(client_connection,)).start()
+    host = "localhost"
+    port = 6379
+    lsock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    lsock.bind((host, port))
+    lsock.listen()
+    print(f"Listening on {(host, port)}")
+    lsock.setblocking(False)
+    sel.register(lsock, selectors.EVENT_READ, data=None)
+
+    try:
+        while True:
+            events = sel.select(timeout=None)
+            for key, mask in events:
+                if key.data is None:
+                    accept_wrapper(key.fileobj)
+                else:
+                    service_connection(key, mask)
+    except KeyboardInterrupt:
+        print("Caught keyboard interrupt, exiting")
+    finally:
+        sel.close()
 
 
 if __name__ == "__main__":
