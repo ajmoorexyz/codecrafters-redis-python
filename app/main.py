e1143433e10d3f572925a866355bd81e8ddf717a
AJ Moore <ajmoore.xyz@gmail.com>
2022-12-27 04:30:26 -0700

    pass 4th stage

--- app/main.py
+++ app/main.py
@@ -4,6 +4,7 @@ import socket
 def main():
     socket.socket()
     server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
+    server_socket.listen(3)  # listen for clients
     client, _ = server_socket.accept()  # wait for client
     while True:
         data = client.recv(1024).decode()  # receive data from client
